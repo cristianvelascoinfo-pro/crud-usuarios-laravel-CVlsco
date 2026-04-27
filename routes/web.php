a6259a49e2170fb39812cb5d246df06b3ce414b8
Cristian Velasco <cristian.velasco.info@gmail.com>
2026-04-26 20:07:15 -0500

    Configuración de ruta raíz para cargar el CRUD directamente

--- routes/web.php
+++ routes/web.php
@@ -4,9 +4,8 @@ use Illuminate\Support\Facades\Route;
 // Importamos nuestro controlador (el cerebro)
 use App\Http\Controllers\UsuarioController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Cambiamos la ruta raíz para que cargue directamente el listado de usuarios
+Route::get('/', [UsuarioController::class, 'index']);
 
 // Esta línea crea automáticamente las 7 rutas del CRUD (index, create, store, etc.)
 Route::resource('usuarios', UsuarioController::class);
